5766df17c42fca50ff8be410cd776bdfdb0108c8
nejmicekk <jarek.dvoracek6@eznam.cz>
2025-02-18 17:58:31 +0100

    Možnost odebrání uživatele + možnost vyhledávání (Admin)

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth import views as auth_views # Je to pro autentizaci
 
 urlpatterns = [
     path('studenti', views.students_list, name='students'),
-    path('', views.users_list, name='users'),
+    path('', views.users_list, name='uzivatele'),
+    path('delete/<int:pk>/', views.odstranit_uzivatele, name="odstranit_uzivatele"),
 ]
--- users/views.py
+++ users/views.py
@@ -10,6 +10,32 @@ def students_list(request):
 
 @login_required(login_url="/login/")
 def users_list(request):
-    users = CustomUser.objects.all()[1:]
-    #jednicka zajistuje, aby se nevypsala prvni prazdna
-    return render(request, 'uzivatele.html', {'users': users})
+    query = request.GET.get("q", "")  # Získáme hodnotu z vyhledávacího pole
+
+    if query:
+        users = CustomUser.objects.filter(jmeno__icontains=query) | CustomUser.objects.filter(prijmeni__icontains=query)
+    else:
+        users = CustomUser.objects.all()[1:]
+
+    return render(request, "uzivatele.html", {"users": users, "query": query})
+
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+@login_required(login_url="/login/")
+def odstranit_uzivatele(request, pk):
+    if not request.user.role == "admin":
+        messages.error(request, "Nemáte oprávnění mazat uživatele.")
+    else:
+        user = get_object_or_404(CustomUser, pk=pk)
+
+        if user == request.user:
+            messages.error(request, "Nemůžete smazat vlastní účet.")
+        elif user.role == "admin":
+            messages.error(request, "Nemůžete smazat administrátora.")
+        else:
+            user.delete()
+            messages.success(request, f"Uživatel {user.username} byl úspěšně odstraněn.")
+
+    return redirect("uzivatele")
